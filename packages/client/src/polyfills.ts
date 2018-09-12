7fb31a9952898ef38ebcddfee9f55a4483798f7e
Didier 'Ptitjes' Villevalois <ptitjes@free.fr>
2018-09-13 00:27:44 +0200

    chore(license): fix missing license headers

--- packages/client/src/polyfills.ts
+++ packages/client/src/polyfills.ts
@@ -1,4 +1,23 @@
 /**
+ * This file is part of CabasVert.
+ *
+ * Copyright 2017, 2018 Didier Villevalois
+ *
+ * CabasVert is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CabasVert is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CabasVert.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
  *
