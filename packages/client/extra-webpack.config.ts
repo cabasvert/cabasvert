79e37ae2c4c7e5b0df9dcdc5894f7171d5e589bb
Didier Villevalois <ptitjes@free.fr>
2024-09-15 20:04:54 +0200

    fix(client): fix polyfills again

--- packages/client/extra-webpack.config.ts
+++ packages/client/extra-webpack.config.ts
@@ -1,11 +1,10 @@
 import { Configuration } from 'webpack'
+import * as NodePolyfillPlugin from 'node-polyfill-webpack-plugin'
 
 export default {
-  resolve: {
-    fallback: {
-      crypto: require.resolve('crypto-browserify'),
-      stream: require.resolve('stream-browserify'),
-      vm: require.resolve('vm-browserify'),
-    }
-  }
+  plugins: [
+    new NodePolyfillPlugin({
+      additionalAliases: ['process', 'crypto', 'stream', 'vm'],
+    }),
+  ],
 } as Configuration
